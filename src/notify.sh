dbfe5444df0a82b10a32e44e69af1e6d0c64f635
shantell <shantell@student.21-school.ru>
2026-09-09 21:16:10 +0300

    init commit

--- src/notify.sh
+++ src/notify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-BOT_TOKEN="5814305143:AAHFYuOrO4pkV3n0jA0SUQoGmPvDqtliRyg"
-USER_ID="556288929"
+BOT_TOKEN="*"
+USER_ID="*"
 
 URL="https://api.telegram.org/bot$BOT_TOKEN/sendMessage"
 TEXT="Stage:+$CI_JOB_STAGE%0A%0AProject:+$CI_PROJECT_NAME%0AStatus:+$CI_JOB_STATUS%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
